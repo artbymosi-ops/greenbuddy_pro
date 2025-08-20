4d95f5b4a7fa84843fe9d74b2d2f3d494d48b80a
artbymosi-ops <art.by.mosi@gmail.com>
2025-08-20 08:40:20 +0200

    Delete src/pages/admin directory

--- src/pages/admin/index.js
+++
@@ -1,2 +0,0 @@
-import Layout from '@/components/Layout';
-export default function Admin(){return(<Layout title="Admin"><div className="card"><p className="subtitle">Admin náhľad a blokovanie príspevkov – čoskoro.</p></div></Layout>);}
