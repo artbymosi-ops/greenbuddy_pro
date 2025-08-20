2f9c22c4374b970d93e8b7e85750ef12c77a7c68
artbymosi-ops <art.by.mosi@gmail.com>
2025-08-20 22:22:20 +0200

    Create impressum.js

---
+++ src/pages/impressum.js
@@ -0,0 +1,9 @@
+export default function Impressum(){
+  return (
+    <div className="card">
+      <h1>Impressum</h1>
+      <p>Angaben gemäß § 5 TMG</p>
+      <p>Firmenname<br/>Straße Nr<br/>PLZ Ort<br/>E-Mail: info@domain.de</p>
+    </div>
+  );
+}
