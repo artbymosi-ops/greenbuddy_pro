26bd017c8fa18083a0886d5a3ff9e771863676ea
artbymosi-ops <art.by.mosi@gmail.com>
2025-08-20 22:22:45 +0200

    Create datenschutz.js

---
+++ src/pages/datenschutz.js
@@ -0,0 +1,8 @@
+export default function Datenschutz(){
+  return (
+    <div className="card">
+      <h1>Datenschutzerklärung</h1>
+      <p>Diese Seite verwendet Supabase und Vercel. Personenbezogene Daten ... (doplníme detaily podľa reality).</p>
+    </div>
+  );
+}
