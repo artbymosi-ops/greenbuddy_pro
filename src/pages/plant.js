31eac5f4d536cfbf156adafdcf0b357112f90674
artbymosi-ops <art.by.mosi@gmail.com>
2025-08-20 06:41:31 +0200

    Create plant.js

---
+++ src/pages/plant.js
@@ -0,0 +1,11 @@
+import Plant from "../src/components/Plant";
+
+export default function PlantPage() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-green-900 to-black text-white">
+      <h1 className="text-3xl mb-6">Moja virtuálna Monstera 🌿</h1>
+      
+      <Plant hydration={70} nutrition={60} xp={120} level={2} pests={0} />
+    </div>
+  );
+}
