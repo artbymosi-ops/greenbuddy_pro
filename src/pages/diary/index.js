32a2f890ab496a697f06fa518bde751f0adb1669
artbymosi-ops <art.by.mosi@gmail.com>
2025-08-21 07:53:10 +0200

    Create index.js

---
+++ src/pages/diary/index.js
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+
+export default function Diary() {
+  return (
+    <Layout title="Tagebuch">
+      <div className="card">
+        <h2 className="title">Tagebuch</h2>
+        <p className="subtitle">Hier kommt dein Pflanzen-Tagebuch.</p>
+      </div>
+    </Layout>
+  );
+}
