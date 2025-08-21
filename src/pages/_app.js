ae9824e8138056b995fed0f1b911270f01cd8f97
artbymosi-ops <art.by.mosi@gmail.com>
2025-08-21 05:31:42 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,5 @@
 import "@/styles/globals.css";
-import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }) {
-  // Voliteľné: PWA service worker (nevadí, ak /sw.js nemáš)
-  useEffect(() => {
-    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sw.js").catch(() => {});
-    }
-  }, []);
-
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
