f3cdfffc557362e10bdf0751aaff43109d068058
artbymosi-ops <art.by.mosi@gmail.com>
2025-08-21 05:48:00 +0200

    Update register.js

--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import { supabase } from '@/utils/supabaseClient'
-
+import Layout from "@/components/Layout";
+export default function Page(){ return <Layout title="Názov"><div className="card"><h2>Názov</h2><p>Obsah príde neskôr.</p></div></Layout>; }
 export default function Register() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
